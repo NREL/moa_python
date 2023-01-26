17919b906315e4b3f7846f015fe7a0dcf0ad31a2
Paul Fleming <pfleming@el3.ib0.cm.hpc.nrel.gov>
2023-01-26 10:20:19 -0700

    Update post-processing scripts

--- moa_python/post_hor_plane.py
+++ moa_python/post_hor_plane.py
@@ -41,47 +41,131 @@ class Post_hor_plane:
         self.summary()
         
     def summary(self):
+        """
+        Print out a brief summary of the horizontal plane
+        """
         
         print(f"Hor_plane has {self.z_N} horizontal planes in {self.num_time_steps} time steps from {self.time[0]} to {self.time[-1]}")
         print(f"Hor_plane levels: {self.z}")
 
+    def get_height_index(self, height):
+        """
+        Return the nearest index to a height
+        
+        Args in:
+            height (float): the height to find
 
-    def get_plane(self, z_idx, t_idx, component = 'u'):
+        Args out:
+            z_idx (float): nearest index
+        """
+        # Identify nearest height
+        z_idx = np.argmin(np.abs(self.z - height))
+        print(f'Nearest height to {height} is {self.z[z_idx]}')
+
+        return z_idx
+
+    def get_time_index(self, time):
+        """
+        Return the nearest time index to a time
+        
+        Args in:
+            time (float): the time to find
+
+        Args out:
+            t_idx (float): nearest index
+        """
+        # Identify nearest height
+        t_idx = np.argmin(np.abs(self.time - time))
+        print(f'Nearest time to {time} is {self.time[t_idx]}')
+
+        return t_idx
+
+
+    def get_plane(self, height, time, component = 'u'):
+        """
+        Get a plane at particular height and time
+        
+        Args in:
+            height (float): the height to get
+            time (float): the time to get
+            component (str): the component to get
+
+        Args out:
+            data (not sure): the horizontal plane
+        """
+
+        z_idx = self.get_height_index(height)
+        t_idx = self.get_time_index(time)
 
         print(f"Returning {component} velocity hor_plane for height {self.z[z_idx]} at time {self.time[t_idx]}")
 
         return self.vel_planes[component][t_idx, :][z_idx*self.x_N*self.y_N:(z_idx+1)*self.x_N*self.y_N].reshape(self.x_N,self.y_N)
 
-    def get_mean_plane(self, z_idx, component = 'u'):
+    def get_mean_plane(self, height, component = 'u'):
+        """
+        Get the mean plane at a particular height
+        
+        Args in:
+            height (float): the height to get
+            component (str): the component to get
+
+        Args out:
+            data (not sure): the horizontal plane
+        """
+
+        z_idx = self.get_height_index(height)
 
         print(f"Returning {component} mean velocity hor_plane for height {self.z[z_idx]}")
 
         mean_plane = np.mean(self.vel_planes[component], axis=0)
         return mean_plane[z_idx*self.x_N*self.y_N:(z_idx+1)*self.x_N*self.y_N].reshape(self.x_N,self.y_N)
 
-    def plot_plane(self, z_idx, t_idx, component = 'u', ax = None):
+    def plot_plane(self, height, time, component = 'u', ax = None, vmin=None, vmax=None):
+        """
+        Plot a horizontal plane at a particular height and time
+        
+        Args in:
+            height (float): the height to plot
+            time (float): the time to plot
+            component (str): the component to plot
+            ax (:py:class:'matplotlib.pyplot.axes', optional):
+                figure axes. Defaults to None.
+            vmin (float, optional) minimum value in colorbar
+            vmax (float, optional) maximum value in colorbar
+        """
 
-        print(f"Plotting {component} velocity hor_plane for height {self.z[z_idx]} at time {self.time[t_idx]}")
+        print(f"Plotting {component} velocity hor_plane for height {height} at time {time}")
 
-        plane = self.get_plane(z_idx, t_idx, component = 'u')
+        plane = self.get_plane(height, time, component = 'u')
 
         if ax is None:
             fig, ax = plt.subplots()
-        im = ax.pcolor(self.x, self.y, plane)
+        im = ax.pcolor(self.x, self.y, plane, vmin=vmin, vmax=vmax)
         ax.set_xlabel('X [m]')
         ax.set_ylabel('Y [m]')
 
         fig.colorbar(im, ax=ax)
 
-    def plot_mean_plane(self, z_idx, component = 'u', ax = None):
+    def plot_mean_plane(self, height, component = 'u', ax = None, vmin=None, vmax=None):
+        """
+        Plot the mean horizontal plane at a particular height and time
+        
+        Args in:
+            height (float): the height to plot
+            component (str): the component to plot
+            ax (:py:class:'matplotlib.pyplot.axes', optional):
+                figure axes. Defaults to None.
+            vmin (float, optional) minimum value in colorbar
+            vmax (float, optional) maximum value in colorbar
+        """
 
-        print(f"Plotting {component} mean velocity hor_plane for height {self.z[z_idx]}")
+        print(f"Plotting {component} mean velocity hor_plane for height {height}")
 
-        plane = self.get_mean_plane(z_idx, component = 'u')
+        plane = self.get_mean_plane(height, component = 'u')
 
         if ax is None:
             fig, ax = plt.subplots()
-        im = ax.pcolor(self.x, self.y, plane)
+        im = ax.pcolor(self.x, self.y, plane, vmin=vmin, vmax=vmax)
         ax.set_xlabel('X [m]')
         ax.set_ylabel('Y [m]')
 
