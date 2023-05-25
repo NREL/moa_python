74a3ad768f09bc7706609e3409245bc4e9c29641
jfrederik-nrel <joeri.frederik@nrel.gov>
2023-05-25 17:28:21 -0600

    Updates to post processing

--- moa_python/post_plane.py
+++ moa_python/post_plane.py
@@ -10,7 +10,7 @@ class Post_plane:
     To do for future: make it compatible for different plane groups.
     """
     
-    def __init__(self, filename):
+    def __init__(self, filename, freq = 1):
         
         # Save the filename
         self.filename = filename
@@ -19,28 +19,32 @@ class Post_plane:
         self.dataset = ncdf.Dataset(filename)
         
         # Read time
-        self.time = np.array(self.dataset.variables['time'])
+        indices = np.arange(0,self.dataset.dimensions['num_time_steps'].size,freq)
+        self.time = np.array(self.dataset.variables['time'][indices])
         self.num_time_steps = len(self.time)
-        
+
         # Save the x and y dimensions
         self.plane = list(self.dataset.groups.keys())[0]
         self.x_N = self.dataset.groups[self.plane].ijk_dims[0]
         self.y_N = self.dataset.groups[self.plane].ijk_dims[1]
         self.x_max = np.sqrt(sum(entry**2 for entry in self.dataset.groups[self.plane].axis1))
         self.y_max = np.sqrt(sum(entry**2 for entry in self.dataset.groups[self.plane].axis2))
+        self.x_dir = self.dataset.groups[self.plane].axis1/self.x_max
+        self.y_dir = self.dataset.groups[self.plane].axis2/self.y_max
+        self.z_dir = self.dataset.groups[self.plane].axis3
         self.x = np.linspace(0, self.x_max, self.x_N)
         self.y = np.linspace(0, self.y_max, self.y_N)
 
         # Save the number of planes 
-        self.z = self.dataset.groups[self.plane].offsets
+        self.z = self.get_plane_location()
         if not isinstance(self.z,np.ndarray): self.z = np.array([self.z])
         self.z_N = len(self.z)
 
         # Save the velocity planes
         self.vel_planes = dict()
-        self.vel_planes['x'] = np.array(self.dataset.groups[self.plane].variables['velocityx'])
-        self.vel_planes['y'] = np.array(self.dataset.groups[self.plane].variables['velocityy'])
-        self.vel_planes['z'] = np.array(self.dataset.groups[self.plane].variables['velocityz'])
+        self.vel_planes['x'] = np.array(self.dataset.groups[self.plane].variables['velocityx'][indices,:])
+        self.vel_planes['y'] = np.array(self.dataset.groups[self.plane].variables['velocityy'][indices,:])
+        self.vel_planes['z'] = np.array(self.dataset.groups[self.plane].variables['velocityz'][indices,:])
         self.vel_planes['u'] = np.sqrt(self.vel_planes['x']**2 + self.vel_planes['y']**2)
         
         # Print a quick summary
@@ -52,21 +56,35 @@ class Post_plane:
         """
         
         print(f"Plane has {self.z_N} plane(s) in {self.num_time_steps} time steps from {self.time[0]} to {self.time[-1]}")
-        print(f"Plane levels: {self.z}")
+        print(f"Plane offsets: {self.z}")
 
-    def get_height_index(self, height):
+    def get_plane_location(self,reference = [0,0,0]):
         """
-        Return the nearest index to a height
+        Return the location of a plane in the third dimension of the defined coordinate system.
+
+        Args in:
+            reference (list??): x, y and z-coordinates in AMR-Wind grid w.r.t. which to calculate plane location, default: [0, 0, 0]
+        """
+
+        if not isinstance(reference,list): reference = list(reference)
+        origin = sum((self.dataset.groups[self.plane].origin-reference)*self.z_dir)
+        self.z = origin + self.dataset.groups[self.plane].offsets
+
+        return self.z
+
+    def get_plane_index(self, z):
+        """
+        Return the nearest index to a position
         
         Args in:
-            height (float): the height to find
+            z (float): the z location to find
 
         Args out:
             z_idx (float): nearest index
         """
-        # Identify nearest height
-        z_idx = np.argmin(np.abs(self.z - height))
-        print(f'Nearest height to {height} is {self.z[z_idx]}')
+        # Identify nearest z
+        z_idx = np.argmin(np.abs(self.z - z))
+        print(f'Nearest plane to {z} is {self.z[z_idx]}')
 
         return z_idx
 
@@ -80,19 +98,19 @@ class Post_plane:
         Args out:
             t_idx (float): nearest index
         """
-        # Identify nearest height
+        # Identify nearest time
         t_idx = np.argmin(np.abs(self.time - time))
         print(f'Nearest time to {time} is {self.time[t_idx]}')
 
         return t_idx
 
 
-    def get_plane(self, height, time, component = 'u'):
+    def get_plane(self, plane, time, component = 'u'):
         """
-        Get a plane at particular height and time
+        Get a plane at particular location and time
         
         Args in:
-            height (float): the height to get
+            plane (float): the plane location to get
             time (float): the time to get
             component (str): the component to get (default: 'u')
             axes (str): the axes of the plane to get (default: 'xy')
@@ -101,26 +119,26 @@ class Post_plane:
             data (not sure): the plane
         """
 
-        z_idx = self.get_height_index(height)
+        z_idx = self.get_plane_index(plane)
         t_idx = self.get_time_index(time)
 
         print(f"Returning {component} velocity plane for slice at {self.z[z_idx]} at time {self.time[t_idx]}")
 
         return self.vel_planes[component][t_idx, :][z_idx*self.x_N*self.y_N:(z_idx+1)*self.x_N*self.y_N].reshape(self.y_N,self.x_N)
 
-    def get_mean_plane(self, height, component = 'u', axes = 'xy'):
+    def get_mean_plane(self, plane, component = 'u', axes = 'xy'):
         """
-        Get the mean plane at a particular height
+        Get the mean plane at a particular location
         
         Args in:
-            height (float): the height to get
+            plane (float): the plane location to get
             component (str): the component to get
 
         Args out:
             data (not sure): the plane
         """
 
-        z_idx = self.get_height_index(height)
+        z_idx = self.get_plane_index(plane)
         x = getattr(self,axes[1]+'_N')
         y = getattr(self,axes[0]+'_N')
 
@@ -143,7 +161,7 @@ class Post_plane:
             y (float): y coordinate of origin
             frame (str): 
                 'python': relative to left bottom corner (default)
-                'amr-wind': AMR-Wind coordinate frame
+                'amr-wind': AMR-Wind coordinate frame (for horizontal planes)
         """
 
         if not 'kwargs' in locals():
@@ -157,18 +175,31 @@ class Post_plane:
             if 'frame' in kwargs.keys(): kwargs.pop('frame')
             for key,value in kwargs.items():
                 setattr(self, key, getattr(self,key)-np.min(getattr(self,key))-value)
-        elif 'frame' in kwargs.keys() and kwargs['frame'] == 'amr-wind':
+        elif kwargs['frame'] == 'amr-wind':
             if not 'x' or not 'y' in kwargs.keys():
                 raise KeyError('For frame amr-wind, both x and y coordinates of desired origin must be given')
-            origin = self.dataset.groups[self.plane].origin
-            angle_field = np.arctan(self.dataset.groups[self.plane].axis1[1]/self.dataset.groups[self.plane].axis1[0])
-            x_dist = kwargs['x']-origin[0]
-            y_dist = kwargs['y']-origin[1]
-            angle = np.arctan(y_dist/x_dist)
-            dist = np.sqrt(x_dist**2 + y_dist**2)
-            new_origin = [dist*np.cos(angle-angle_field)+np.min(self.x), dist*np.sin(angle-angle_field)+np.min(self.y)]
-            self.x = self.x - new_origin[0]
-            self.y = self.y - new_origin[1]
+            if hasattr(self,'unit') and self.unit == 'D':
+                print('WARNING: "set_origin" is meant to be used before using "scale_to_rot_diam". Expect undesired results!')
+            if not hasattr(self,'amr_origin'): self.amr_origin = self.dataset.groups[self.plane].origin[0:2]
+            shift_in_origin = np.array([kwargs['x'], kwargs['y']]) - self.amr_origin
+            self.amr_origin = np.array([kwargs['x'], kwargs['y']])
+            normal = self.dataset.groups[self.plane].axis1/np.sqrt(self.dataset.groups[self.plane].axis1.dot(self.dataset.groups[self.plane].axis1))
+            self.x = self.x - (normal[0]*shift_in_origin[0]+normal[1]*shift_in_origin[1])
+            self.y = self.y - (normal[0]*shift_in_origin[1]-normal[1]*shift_in_origin[0])
+
+    def scale_to_rot_diam(self,rot_diam):
+        """
+        Scales all axes to rotor diameter. 
+
+        Args in:
+            rot_diam: rotor diameter in m
+        """
+
+        if not hasattr(self,'unit') or self.unit == 'm':
+            self.x = self.x/rot_diam
+            self.y = self.y/rot_diam
+            self.z = self.z/rot_diam
+            self.unit = 'D'
 
     def plot_plane(self, z, time, component = 'u', ax = None, vmin=None, vmax=None):
         """
@@ -194,8 +225,9 @@ class Post_plane:
             plt.sca(ax)
             fig = plt.gcf()
         im = ax.pcolor(self.x, self.y, plane, vmin=vmin, vmax=vmax)
-        ax.set_xlabel('X [m]')
-        ax.set_ylabel('Y [m]')
+        if not hasattr(self,'unit'): self.unit = 'm'
+        ax.set_xlabel(f'X [{self.unit}]')
+        ax.set_ylabel(f'Y [{self.unit}]')
         ax.set_aspect('equal')
 
         fig.colorbar(im,ax=ax,location='bottom')
@@ -210,7 +242,7 @@ class Post_plane:
             z (float): the z-coordinate of the plane to plot
             component (str): the component to plot
             ax (:py:class:'matplotlib.pyplot.axes', optional):
-                figure axes. Defaults to None.
+                figure axis. Defaults to None.
             vmin (float, optional) minimum value in colorbar
             vmax (float, optional) maximum value in colorbar
         """
@@ -225,13 +257,42 @@ class Post_plane:
             plt.sca(ax)
             fig = plt.gcf()
         im = ax.pcolor(self.x, self.y, plane, vmin=vmin, vmax=vmax)
-        ax.set_xlabel('X [m]')
-        ax.set_ylabel('Y [m]')
+        if not hasattr(self,'unit'): self.unit = 'm'
+        ax.set_xlabel(f'X [{self.unit}]')
+        ax.set_ylabel(f'Y [{self.unit}]')
         ax.set_aspect('equal')
 
         fig.colorbar(im, ax=ax,location='bottom')
 
         return ax
+    
+    def plot_turbine(self,hub_height=150/240,rot_diam=1,turb_loc=[0,0],ax=None,plane='xy'):
+        """
+        Plot the turbine location in the flow field
+
+        Args in:
+            hub_height (float): turbine hub height, defaults to 240/150
+            rot_diam (float): rotor diameter, defaults to 1
+            turb_loc (list, tuple, np.ndarray): x and y turbine location in flow field
+                Defaults to [0, 0]
+            ax (:py:class:'matplotlib.pyplot.axes', optional): 
+                figure axis. Defaults to None.
+            plane (string): plane in which to plot turbine ('xy', 'yz', or 'xz')
+        """
+
+        if ax is None: ax=plt.gca()
+
+        if plane == 'yz':
+            ax.plot(rot_diam/2*np.sin(np.linspace(0, 2*np.pi,200)),\
+                    hub_height+rot_diam/2*np.cos(np.linspace(0, 2*np.pi,200)),'k-',linewidth=1)
+        elif plane == 'xy':
+            ax.plot([turb_loc[0],turb_loc[0]],\
+                    [turb_loc[1]-rot_diam/2,turb_loc[1]+rot_diam/2],'k',linewidth=1.5)
+        elif plane == 'xz':
+            ax.plot([turb_loc[0],turb_loc[0]],\
+                    hub_height+[turb_loc[1]-rot_diam/2,turb_loc[1]+rot_diam/2],'k',linewidth=1.5)
+
+
 
 
 
